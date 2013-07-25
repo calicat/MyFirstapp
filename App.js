a09fb63fc10cf2367f6ca90290e788cb4fbba533
Kathy <kmcclung@rallydev.com>
2013-07-25 11:57:33 -0400

    This is the first rendition

---
+++ App.js
@@ -0,0 +1,10 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+
+    launch: function() {
+        //Write app code here
+        console.log("here world +1");
+        console.log("here goood stuff");
+    }
+});
